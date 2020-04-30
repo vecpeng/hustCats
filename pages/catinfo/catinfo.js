c05af6c2060a023f652253df8893f79f0a2899fe
xiaocnscl <841298391@qq.com>
2020-05-01 01:28:17 +0800

    hustcats2.0

--- pages/catinfo/catinfo.js
+++ pages/catinfo/catinfo.js
@@ -1,23 +1,367 @@
 const app = getApp()
 
+let swiperImgPage = 1
+let id = 0
+let currentIndex = 0
+let currentDate = ''
+let lastDate = ''
+let imgsPage = 1
+let openID = ''
+let inputDesc = ''
+let recordPage = 1
+
 Page({
-  data:{
-    catSwiperImgs: {}
+  data: {
+    catSwiperImgs: [],
+    catInfo: {},
+    catImgs: [],
+    photoNum: 0,
+    records: [],
+    recordNum: 0,
+    userInfo: null,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    hasUserInfo: false,
+    tab: 'album',
+    uploadImgUrl: '/img/add.png',
+    uploadContainerDisplay: 'none',
+    infoDisplay: 'blcok',
+    bigImgDisplay: 'none',
   },
   onLoad: function(option){
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } 
     let self = this
+    id = option.id
     wx.request({
-      url: 'https://wxapi.ufatfat.com/cat/getCatSwiperImgsById',
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatSwiperImgsById',
       data: {
-        id: option.id
+        id: option.id,
+        page: swiperImgPage
       },
       success(res){
-        console.log(res.data)
-        console.log(res.data[0])
         self.setData({
           catSwiperImgs: res.data
         })
+      },      
+    })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getPhotoNum',
+      data: {
+        id: option.id,
+      },
+      success(res){
+        self.setData({
+          photoNum: res.data,
+        })
       }
     })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatInfoById',
+      data: {
+        id: option.id,
+      },
+      success(res){
+        self.setData({
+          catInfo: res.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatImgsById',
+      data: {
+        id: option.id,
+        page: imgsPage
+      },
+      success(res){
+        console.log(res.data)
+        self.setData({
+          catImgs: res.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getRecordNum',
+      data: {
+        id: id,
+      },
+      success(res){
+        self.setData({
+          recordNum: res.data
+        })
+      }
+    })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getRecords',
+      data: {
+        id: id,
+        page: recordPage
+      },
+      success(res){
+        self.setData({
+          records: res.data
+        })
+      }
+    })
+  },
+  bigImg: function(e){
+    let self = this
+    console.log(e)
+    console.log(e.currentTarget.dataset.id)
+    console.log(self.data)
+    wx.previewImage({
+      current: self.data.catImgs[e.currentTarget.dataset.id].img,
+      urls: [self.data.catImgs[e.currentTarget.dataset.id].img],
+    })
+  },
+  /*bigImg: function(e){
+    let self = this
+    console.log(e.currentTarget.dataset.id)
+    console.log(self.data)
+    wx.previewImage({
+      current: self.data.catImgs[e.currentTarget.dataset.id].img,
+      urls: [self.data.catImgs[e.currentTarget.dataset.id].img],
+    })
+  },*/
+  recordBigImg: function(e){
+    let self = this
+    console.log(e.currentTarget.dataset.id)
+    console.log(self.data)
+    wx.previewImage({
+      current: self.data.records[e.currentTarget.dataset.id].img,
+      urls: [self.data.records[e.currentTarget.dataset.id].img],
+    })
+  },
+  loadNew: function(e){
+    let self = this
+    let imgsLen = jsonLength(self.data.catSwiperImgs)
+    if(e.detail.current == (imgsLen - 1)){
+      swiperImgPage++
+      wx.request({
+        url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatSwiperImgsById',
+        data: {
+          id: id,
+          page: swiperImgPage
+        },
+        success(res){
+          if(res.data){
+            self.setData({
+              catSwiperImgs: self.data.catSwiperImgs.concat(res.data)
+            })
+          }
+        }
+      })
+    }
+  },
+  onReachBottom: function(e){
+    let self = this
+    if(this.data.tab == 'album'){
+      imgsPage++
+      wx.request({
+        url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatImgsById',
+        data: {
+          id: id,
+          page: imgsPage
+        },
+        success(res){    
+          if(res.data){
+            self.setData({
+              catImgs: self.data.catImgs.concat(res.data)
+            })        
+          }
+        },
+      })
+    }else{
+      recordPage++
+      wx.request({
+        url: 'https://wxapi.ufatfat.com/hustcats/cat/getRecords',
+        data: {
+          id: id,
+          page: recordPage
+        },
+        success(res){    
+          if(res.data){
+            self.setData({
+              records: self.data.records.concat(res.data)
+            })        
+          }
+        },
+      })
+    }
+  },
+  getUserInfo: function(e){
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+      uploadContainerDisplay: 'block',
+    })
+  },
+  changeTab: function(e){
+    let thatTab = e.currentTarget.dataset.tab
+    if(this.data.tab == thatTab)
+      return
+    this.setData({
+      tab: thatTab
+    })
+    console.log(this.data)
+  },
+  chooseImg: function(){
+    let self = this
+    wx.chooseImage({
+      count: 1,
+      success(res){
+        let tempPaths = res.tempFilePaths
+        self.setData({
+          uploadImgUrl: res.tempFilePaths[0]
+        })
+      }
+    })
+  },
+  updateInput: function(e){
+    inputDesc = e.detail.value
+  },
+  uploadImg: function(){
+    let self = this
+    if(this.data.uploadImgUrl == '/img/add.png'){
+      wx.showToast({
+        title: '还没选择图片！',
+        image: '/img/cross.png',
+        duration: 2000,
+      })
+      return 
+    }
+    if(!openID){
+      wx.login({
+        success(res){
+          console.log(self.data)
+          wx.request({
+            url: 'https://wxapi.ufatfat.com/hustcats/user/getOpenID',
+            data: {
+              jscode: res.code
+            },
+            success(res){
+              if(res.data)
+                openID = res.data
+                console.log(res.data)
+                wx.uploadFile({
+                  filePath: self.data.uploadImgUrl,
+                  name: 'uploadImg',url: 'https://wxapi.ufatfat.com/hustcats/user/uploadRecord',
+                  formData: {
+                    'desc': inputDesc,
+                    'openID': openID,
+                    'catid': id,
+                    'avatar': self.data.userInfo.avatarUrl,
+                    'nickname': self.data.userInfo.nickName,
+                  },
+                  success(res){
+                    console.log(res.data)
+                    if(res.data == 1){
+                      wx.showToast({
+                        title: '提交成功',
+                        icon: 'success',
+                        duration: 2000
+                      })
+                      self.setData({
+                        uploadContainerDisplay: 'none'
+                      })
+                    }else{
+                      console.log(res.data)
+                      wx.showToast({
+                        title: res.data,
+                        image: '/img/cross.png',
+                        duration: 2000
+                      })
+                    }
+                  }      
+                })     
+            }
+          })
+        }
+      })
+    }else{
+      wx.login({
+        success(res){
+          console.log(self.data)
+          wx.request({
+            url: 'https://wxapi.ufatfat.com/hustcats/user/getOpenID',
+            data: {
+              jscode: res.code
+            },
+            success(res){
+              if(res.data)
+                openID = res.data
+                console.log(res.data)
+                wx.uploadFile({
+                  filePath: self.data.uploadImgUrl,
+                  name: 'uploadImg',url: 'https://wxapi.ufatfat.com/hustcats/user/uploadRecord',
+                  formData: {
+                    'desc': inputDesc,
+                    'openID': openID,
+                    'catid': id,
+                    'avatar': self.data.userInfo.avatarUrl,
+                    'nickname': self.data.userInfo.nickName,
+                  },
+                  success(res){
+                    console.log(res.data)
+                    if(res.data == 1){
+                      wx.showToast({
+                        title: '提交成功',
+                        icon: 'success',
+                        duration: 2000
+                      })
+                      self.setData({
+                        uploadContainerDisplay: 'none'
+                      })
+                    }else{
+                      console.log(res.data)
+                      wx.showToast({
+                        title: res.data,
+                        image: '/img/cross.png',
+                        duration: 2000
+                      })
+                    }
+                  }      
+                })     
+            }
+          })
+        }
+      })
+    }
+  },
+  toggleDisplay: function(){
+    console.log(this.data)
+    if(this.data.uploadContainerDisplay == 'none')
+      this.setData({
+        uploadContainerDisplay: 'block'
+      })
+    else
+      this.setData({
+        uploadContainerDisplay: 'none'
+      })
   }
 })
+
+function jsonLength(o){
+  let len = 0;
+  for(let p in o){
+    len++
+  }
+  return len
+}
+function upload(){
+  
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,63 +2,219 @@
 //获取应用实例
 const app = getApp()
 let page = 1
+let thisPage = 'index'
 
 Page({
   data: {
-    catsInfo: {}
+    catsInfo: {},
+    modelDisplay: 'none',
+    filterDisplay: 'none',
+    locationDisplay: 'block',
+    speciesDisplay: 'none',
+    locationChosen: 'chosen',
+    speciesChosen: '',
+    locationFilter: [],
+    speciesFilter: [],
+    locationFilters: [],
+    speciesFilters: [],
+    locationFiltersChosen: ['', '', ''],
+    speciesFiltersChosen: [],
+    locationAll: 'filterChosen',
+    speciesAll: 'filterChosen',
+    tab: 'index',
   },
   searchSubmit: function(e){
-    let self = this
+   
     wx.request({
-      url: 'https://wxapi.ufatfat.com/cat/searchCats',
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/searchCats',
       data: {
         name: e.detail.value
       },
-      success(res){
-        self.setData({
+      success:(res)=>{//利用箭头函数替换原来的that=this
+        console.log(res)
+        this.setData({
           catsInfo: res.data
         })
       }
     })
   },
   jumpto: function(e){
-    wx.redirectTo({
+    wx.navigateTo({
       url: '/pages/catinfo/catinfo?id=' + e.currentTarget.dataset.id,
     })
-    console.log(e)
+  },
+  locationChosen: function(){
+    if(!this.data.locationChosen)
+      this.setData({
+        locationChosen: 'chosen',
+        speciesChosen: '',
+        locationDisplay: 'block',
+        speciesDisplay: 'none',
+      })
+  },
+  speciesChosen: function(){
+    if(!this.data.speciesChosen)
+      this.setData({
+        locationChosen: '',
+        speciesChosen: 'chosen',
+        locationDisplay: 'none',
+        speciesDisplay: 'block',
+      })
+  },
+  toggleLocationFilter: function(e){
+    if(e.currentTarget.dataset.id == 'all'){
+      this.setData({
+        locationAll: 'filterChosen',
+        locationFiltersChosen: []
+      })
+      return
+    }
+    let data = this.data.locationFiltersChosen
+    data[e.currentTarget.dataset.id-1] = data[e.currentTarget.dataset.id-1] ? null : 'filterChosen'
+    if(data.indexOf('filterChosen') == -1)
+      this.setData({
+        locationAll: 'filterChosen'
+      })
+    else
+      this.setData({
+        locationAll: ''
+      })
+    let filters = this.data.locationFilter
+    filters[e.currentTarget.dataset.id] = !filters[e.currentTarget.dataset.id] ? e.currentTarget.dataset.id : null
+    this.setData({
+      locationFilter: filters,
+      locationFiltersChosen: data
+    })
+  },
+  toggleSpeciesFilter: function(e){
+    if(e.currentTarget.dataset.id == 'all'){
+      this.setData({
+        speciesAll: 'filterChosen',
+        speciesFiltersChosen: []
+      })
+      return
+    }
+    let data = this.data.speciesFiltersChosen
+    data[e.currentTarget.dataset.id-1] = data[e.currentTarget.dataset.id-1] ? null : 'filterChosen'
+    if(data.indexOf('filterChosen') == -1)
+      this.setData({
+        speciesAll: 'filterChosen'
+      })
+    else
+      this.setData({
+        speciesAll: ''
+      })
+    let filters = this.data.speciesFilter
+    filters[e.currentTarget.dataset.id] = !filters[e.currentTarget.dataset.id] ? e.currentTarget.dataset.id : null
+    this.setData({
+      speciesFilter: filters,
+      speciesFiltersChosen: data
+    })
+  },
+  toggleFilter: function(){
+    if(this.data.modelDisplay == 'none')
+      this.setData({
+        modelDisplay: 'block',
+      })
+    else
+      this.setData({
+        modelDisplay: 'none',
+      })
+      if(this.data.filterDisplay == 'none')
+        this.setData({
+          filterDisplay: 'block',
+        })
+      else
+        this.setData({
+          filterDisplay: 'none',
+        })
+      
+  },
+  btnReset: function(){
+    this.setData({
+      locationAll: 'filterChosen',
+      locationFiltersChosen: [],
+      speciesAll: 'filterChosen',
+      speciesFiltersChosen: [],
+      locationFilter: [],
+      speciesFilter: []
+    })
+  },
+  btnDone: function(){
+   
+    let locationFilter = this.data.locationFilter ? this.data.locationFilter.join(',').replace(/,{2,}/g, ',').substr(1) : ''
+    let speciesFilter = this.data.speciesFilter ? this.data.speciesFilter.join(',').replace(/,{2,}/g, ',').substr(1) : ''
+    page = 1
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatsInfo',
+      data: {
+        page: page,
+        locationFilter: locationFilter,
+        speciesFilter: speciesFilter
+      },
+      success:res=>{        
+        console.log(res.data)
+        this.setData({
+          catsInfo: res.data,
+          filterDisplay: 'none'
+        })        
+      },
+    })
+
   },
   onLoad: function(){
-    let self = this
-    console.log(self.data)
+   
     wx.request({
-      url: 'https://wxapi.ufatfat.com/cat/getCatsInfo',
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatsInfo',
       data: {
-        page: page
+        page: page,
+        locationFilter: this.data.locationFilter.join(','),
+        speciesFilter: this.data.speciesFilter.join(',')
       },
-      success(res){        
-        self.setData({
+      success:res=>{        
+        this.setData({
           catsInfo: res.data
         })        
-        console.log(self.data)
       },
+    })
+    wx.request({
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getFilters',
+      success:res=>{
+        this.setData({
+          locationFilters: res.data.locationFilters,
+          speciesFilters: res.data.speciesFilters
+        })
+      }
     })
   },
   onReachBottom: function(e){
     page++
-    let self = this
+ 
     wx.request({
-      url: 'https://wxapi.ufatfat.com/cat/getCatsInfo',
+      url: 'https://wxapi.ufatfat.com/hustcats/cat/getCatsInfo',
       data: {
-        page: page
+        page: page,
+        locationFilter: this.data.locationFilter.join(','),
+        speciesFilter: this.data.speciesFilter.join(',')
       },
-      success(res){    
-        self.setData({
-          catsInfo: self.data.catsInfo.concat(res.data)
-        })        
-        console.log(self.data)
+      success:res=>{    
+        if(res.data){
+          this.setData({
+            catsInfo: this.data.catsInfo.concat(res.data)
+          })        
+        }
       },
     })
-  }
+  },
+  changeTab: function(e){
+    let thatTab = e.currentTarget.dataset.tab
+    if(this.data.tab == thatTab)
+      return
+    this.setData({
+      tab: thatTab
+    })
+    console.log(this.data)
+  },
 })
 
 function jsonAdd(d, s){
